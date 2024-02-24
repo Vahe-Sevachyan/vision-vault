4406ceb5bbe624584ecc890628b827c1e9fb766b
Vahe Sevachyan <vscode13@gmail.com>
2024-02-23 23:49:24 -0800

    added overlay styling

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,11 +1,19 @@
 <template>
   <main>
+    <div class="overlay"></div>
     <div class="container">
       <header>
         <h1>Vision Vault</h1>
         <button>+</button>
       </header>
       <div class="cards-container">
+        <div class="card">
+          <p class="main-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure porro
+            fuga explicabo, officia incidunt qui?
+          </p>
+          <p class="date">02/22/2022</p>
+        </div>
         <div class="card">
           <p class="main-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure porro
@@ -59,5 +67,23 @@ button {
   justify-content: space-between;
   margin-right: 20px;
   margin-bottom: 20px;
+}
+.main-text {
+  margin-bottom: 110px;
+}
+
+.cards-container {
+  display: flex;
+  flex: wrap;
+}
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.77);
+  z-index: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
